cb57f4538a06b54acd4b4c5c0a2bb907716e968d
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2021-12-21 16:01:38 -0800

    base layer

---
+++ index.js
@@ -0,0 +1,15 @@
+//create key > method 1
+let key="name";
+let keyValue = "Kyle";
+localStorage.setItem(key, keyValue);
+
+//create key > method 2
+localStorage.setItem("age","21");
+
+//create key > method 3
+localStorage.nationality = "American";
+
+/*
+TO update value of an existing key-value pair, write code like you're recreating it.
+It will overrite it
+*/
