090149bf26875bfa9373748f4858258b27fa8c3c
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2021-12-21 17:36:58 -0800

    added cookies

--- index.js
+++ index.js
@@ -61,3 +61,45 @@ delete sessionStorage.sad;
 
 //delete all
 sessionStorage.clear();
+//----------------COOKIES ------------------------------
+
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  (function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+       let age = prompt("Please enter your age:","");
+       setCookie("age", age, 30);
+
+       let favoriteMusic = prompt("Enter favorite genre","");
+       setCookie("faveGenre", favoriteMusic, 30);
+    }
+  })();
